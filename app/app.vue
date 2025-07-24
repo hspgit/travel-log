31217fd394f3a9d7fcc64ece2fda4973afa4fd08
Hrishikesh Pradhan <hspsat@gmail.com>
2025-07-23 23:56:35 -0400

    :sparkles: setup tailwind and daisy UI

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-4xl font-bold text-center mt-10 uppercase">
+      <span class="text-blue-500">Nuxt 4</span> + <span class="text-green-500">Tailwind CSS</span> =
+      Hello world!
+      <button class="btn btn-secondary">
+        View Map <Icon name="tabler:map" />
+      </button>
+    </h1>
   </div>
 </template>
