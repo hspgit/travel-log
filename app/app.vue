95a1be9750d9af217504561ef26325c8031b0df6
Hrishikesh Pradhan <hspsat@gmail.com>
2025-07-27 00:07:10 -0400

    Sign In buttons and general layout :wrench:

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-4xl font-bold text-center mt-10 uppercase">
-      <span class="text-blue-500">Nuxt 4</span> + <span class="text-green-500">Tailwind CSS</span> =
-      Hello world!
-      <button class="btn btn-secondary">
-        View Map <Icon name="tabler:map" /> <Icon name="svg-spinners:blocks-wave" size="24" />
-      </button>
-    </h1>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
+import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  css: ["~~/assets/app.css"],
   modules: ["@nuxt/eslint", "@nuxt/icon"],
+  css: ["~/assets/app.css"],
   vite: {
     plugins: [tailwindcss()],
   },
