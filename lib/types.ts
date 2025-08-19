04f14305bb6ff82ae7c541aceef4faba50005d55
Hrishikesh Pradhan <hspsat@gmail.com>
2025-08-19 13:23:10 -0400

    :art: Display added locations for logged in user

--- lib/types.ts
+++ lib/types.ts
@@ -1,10 +1,7 @@
-import type { User } from "better-auth";
+import type { UserWithId } from "./auth";
 
 declare module "h3" {
-    // eslint-disable-next-line ts/consistent-type-definitions
-    interface H3EventContext {
-        user?: Omit<User, "id"> & {
-            id: number;
-        };
-    }
+    type H3EventContext = {
+        user?: UserWithId;
+    };
 }
