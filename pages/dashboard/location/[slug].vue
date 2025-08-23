f3a958bd9ca33e635506bebd916ddd03df77f5d0
Hrishikesh Pradhan <hspsat@gmail.com>
2025-08-22 23:29:10 -0400

    :rotating_light: Delete location impl

--- pages/dashboard/location/[slug].vue
+++ pages/dashboard/location/[slug].vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import type { FetchError } from "ofetch";
+
 const locationStore = useLocationStore();
 const {
     currentLocation: location,
@@ -6,7 +8,14 @@ const {
     currentLocationError: error,
 } = storeToRefs(locationStore);
 
+const isOpen = ref(false);
+const deleteError = ref("");
+const isDeleting = ref(false);
+
 const route = useRoute();
+
+const loading = computed(() => status.value === "pending" || isDeleting.value);
+const errorMessage = computed(() => error.value?.statusMessage || deleteError.value);
 
 onMounted(() => {
     locationStore.refreshCurrentLocation();
@@ -17,23 +26,74 @@ onBeforeRouteUpdate((to) => {
         locationStore.refreshCurrentLocation();
     }
 });
+
+function openDialog() {
+    isOpen.value = true;
+    (document.activeElement as HTMLAnchorElement).blur();
+}
+
+async function confirmDelete() {
+    try {
+        isOpen.value = false;
+        deleteError.value = "";
+        isDeleting.value = true;
+        await $fetch(`/api/locations/${route.params.slug}`, {
+            method: "DELETE",
+        });
+        navigateTo("/dashboard");
+    }
+    catch (e) {
+        const error = e as FetchError;
+        deleteError.value = getFetchErrorMessage(error);
+    }
+    isDeleting.value = false;
+}
 </script>
 
 <template>
     <div class="p-4 min-h-64">
-        <div v-if="status === 'pending'">
+        <div v-if="loading">
             <div class="loading loading-infinity" />
         </div>
 
-        <div v-if="error && status !== 'pending'" class="alert alert-error">
+        <div v-if="errorMessage && !loading " class="alert alert-error">
             <h2 class="text-lg">
-                {{ error.statusMessage }}
+                {{ errorMessage }}
             </h2>
         </div>
-        <div v-if="route.name === 'dashboard-location-slug' && location && status !== 'pending'">
+        <div v-if="route.name === 'dashboard-location-slug' && location && !loading">
             <h2 class="text-xl">
                 {{ location.name }}
+                <div class="dropdown dropdown-bottom">
+                    <div
+                        tabindex="0"
+                        role="button"
+                        class="btn m-1 p-0"
+                    >
+                        <Icon size="20" name="tabler:dots-vertical" />
+                    </div>
+                    <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li>
+                            <NuxtLink @click="openDialog">
+                                <Icon name="tabler:trash-x-filled" size="20" />
+                                Delete
+                            </NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink
+                                :to="{
+                                    name: 'dashboard-location-slug-edit',
+                                    params: { slug: route.params.slug },
+                                }"
+                            >
+                                <Icon name="tabler:map-pin-cog" size="20" />
+                                Edit
+                            </NuxtLink>
+                        </li>
+                    </ul>
+                </div>
             </h2>
+
             <p class="text text-sm">
                 {{ location.description }}
             </p>
@@ -50,5 +110,14 @@ onBeforeRouteUpdate((to) => {
         <div v-if="route.name !== 'dashboard-location-slug' ">
             <NuxtPage />
         </div>
+        <AppDialog
+            title="Are you sure?"
+            description="Deleting this location will also delete all of the associated logs. This cannot be undone. Do you really want to do this?"
+            confirm-label="Yes, delete this location!"
+            confirm-class="btn-error"
+            :is-open="isOpen"
+            @on-closed="isOpen = false"
+            @on-confirmed="confirmDelete"
+        />
     </div>
 </template>
