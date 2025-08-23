14c2d3c95c97bf93987152e3d16de317e8043503
Hrishikesh Pradhan <hspsat@gmail.com>
2025-08-23 18:46:06 -0400

    🍻 User can edit location log

--- pages/dashboard/location/[slug]/[id].vue
+++ pages/dashboard/location/[slug]/[id].vue
@@ -13,6 +13,12 @@ const errorMessage = computed(() => error.value?.statusMessage);
 onMounted(() => {
     locationStore.refreshCurrentLocationLog();
 });
+
+onBeforeRouteUpdate((to) => {
+    if (to.name === "dashboard-location-slug-id") {
+        locationStore.refreshCurrentLocationLog();
+    }
+});
 </script>
 
 <template>
@@ -70,6 +76,9 @@ onMounted(() => {
             <p class="text text-sm">
                 {{ locationLog.description }}
             </p>
+        </div>
+        <div v-else>
+            <NuxtPage />
         </div>
     </div>
 </template>
